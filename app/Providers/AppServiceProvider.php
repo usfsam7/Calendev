6bfc570f369f715452f53f3252372911ddbb0853
youssef <yousameh2006@gmail.com>
2025-07-28 15:38:26 +0300

    production issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    // app/Providers/AppServiceProvider.php
+    public function boot()
     {
-        //
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
